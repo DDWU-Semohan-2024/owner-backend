e105951a7059e994bf51e8bc27a72e9b085e9d93
sjchae <soojch12@gmail.com>
2024-05-12 11:47:41 +0900

    Feat: Auth Controller 아이디 찾기, 비밀번호 변경 코드 복구

--- src/main/java/semohan/owner/domain/owner/api/AuthController.java
+++ src/main/java/semohan/owner/domain/owner/api/AuthController.java
@@ -4,9 +4,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import semohan.owner.domain.owner.application.AuthService;
+import semohan.owner.domain.owner.dto.ResetPasswordRequestDto;
 import semohan.owner.domain.owner.dto.SignInDto;
 
 @Slf4j
@@ -28,5 +31,22 @@ public class AuthController {
     public ResponseEntity<Boolean> signOut(HttpServletRequest httpServletRequest) {
         httpServletRequest.getSession().invalidate();
         return ResponseEntity.ok(true);
+    }
+
+    @PostMapping(value = "/find-id")
+    public ResponseEntity<String> findUserName(@RequestParam("phoneNumber") String phoneNumber) {
+        String userName = authService.findUserName(phoneNumber);
+        if (userName != null) {
+            // Owner가 존재하는 경우
+            return ResponseEntity.ok(userName);
+        } else {
+            // Owner가 존재하지 않는 경우
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("사용자가 존재하지 않습니다.");
+        }
+    }
+
+    @PostMapping(value="/reset-password")
+    public ResponseEntity<Boolean> resetPassword(@RequestBody @Validated ResetPasswordRequestDto request) {
+        return ResponseEntity.ok(authService.resetPassword(request));
     }
 }
