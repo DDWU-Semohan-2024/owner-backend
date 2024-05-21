59785e170bb08fd1dee8a7d83cace3f62aa4317c
sjchae <soojch12@gmail.com>
2024-05-21 22:18:30 +0900

    Feat: "SMS 인증 서비스 구현"

--- src/main/java/semohan/owner/domain/owner/application/SmsService.java
+++ src/main/java/semohan/owner/domain/owner/application/SmsService.java
@@ -16,6 +16,8 @@ public class SmsService {
     private String apiKey;
     @Value("${coolsms.api.secret}")
     private String apiSecretKey;
+    @Value("${sms.senderNumber}")
+    String senderNumber;
 
     private DefaultMessageService messageService;
 
@@ -28,7 +30,6 @@ public class SmsService {
     public SingleMessageSentResponse sendOne(String to, String verificationCode) {
         Message message = new Message();
         // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
-        String senderNumber = "01073728895";
         message.setFrom(senderNumber);
         message.setTo(to);
         message.setText("[세모한] 아래의 인증번호를 입력해주세요\n" + verificationCode);
