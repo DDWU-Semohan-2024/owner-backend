1ad53cfb7d3362d0e09516e4ea75704f28e12669
Songhee120 <81075657+Songhee120@users.noreply.github.com>
2024-06-06 01:47:29 +0900

    update: http 요청 관련 수정

--- src/main/java/semohan/owner/domain/restaurant/dto/RestaurantInfoUpdateDto.java
+++ src/main/java/semohan/owner/domain/restaurant/dto/RestaurantInfoUpdateDto.java
@@ -21,4 +21,5 @@ public class RestaurantInfoUpdateDto {
     private String address;
 
     private String detailedAddress;
+
 }
--- src/main/java/semohan/owner/global/config/WebConfig.java
+++ src/main/java/semohan/owner/global/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8088", "http://localhost:3000") // 허용할 출처
-                .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP method
+                .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000);          // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
